56af090a5e67d8c6b840d11898e40b30d81b262d
hirasawa <ddt.jissyusei3@gmail.com>
2017-06-22 20:58:47 +0900

    コードのロジックを修正しています

--- test/test.cpp
+++ test/test.cpp
@@ -32,12 +32,16 @@ int main(int argc,const char* argv[])
 	ifstream ifstream_fail(argv[1]);											//素データ アドレス 細井延明
 	ofstream ofstream_fail(argv[2]);											//出力ファイル名 コマンドライン引数で指定する
 
-	string change_text1("FN:");													//既存データに会社名を追加 するファクターを定義
-	string set_text1("ゼネテック)");											//既存データに会社名を追加
+	string change_text1("N:");													//既存データに会社名を追加 するファクターを定義
+	string change_text11("ORG:");
+	string change_text111("株式会社");
+	string company_set("社名読み: ");											//会社名を設定するファクター
+	string company_search("グループ: ");										// \n 文字を検出できないのでその次の文字列を検出します
+	string set_text1(" ");														//既存データに会社名を追加
 
 	string insert_place2("X-PHONETIC-FIRST-NAME:");								//この文字列を検出したならばsort_string	を追加する
 
-	string change_text2("SORT-STRING:ゼネテック ");								//ソートタグを新たに追加する
+	string change_text2("SORT-STRING:");										//ソートタグを新たに追加する
 
 	string change_text3("X-PHONETIC-LAST-NAME:");								//既存データに会社名を追加するファクターを定義
 
@@ -55,18 +59,64 @@ int main(int argc,const char* argv[])
 		//会社名を追加する条件に合致するならば書き込み パターン1
 		if(!text_copy[i].find(change_text1)) {
 
-			//FN: のところに会社名を追加します
-			text_copy[i].insert(3,set_text1);
+			//各個人の最初の項目 を検出した際会社名を更新する
+			for(int j=i;; j++) {
+
+				//社名読みを検出するまで操作します
+				if(text_copy[j].rfind(change_text11) != -1) {
+
+					int len1 = text_copy[j].rfind(change_text11);				//ORG読みまでの先端からの長さ
+
+					string tmp = change_text11;
+					tmp += change_text111;
+
+					if(text_copy[j].rfind(tmp) != -1) {
+
+						set_text1 = text_copy[j].substr(len1 + change_text11.length() + change_text111.length());
+
+					} else {
+
+						set_text1 = text_copy[j].substr(len1 + change_text11.length(),
+								text_copy[j].length() - change_text11.length() - change_text111.length());
+					}
+
+					break;
+				}
+			}
+
+			//N: のところに会社名を追加します
+			text_copy[i].insert(2,set_text1);
+			text_copy[i].insert(2 + set_text1.length(),") ");
+
 
 		//会社名を追加する条件に合致するならば書き込み パターン2
 		} else if(!text_copy[i].find(change_text3)) {
 
 			//X-PHONETIC-LAST-NAME: のところに会社名を追加します
-			text_copy[i].insert(21,set_text1);
+			text_copy[i].insert(change_text3.length(),set_text1);
+			text_copy[i].insert(change_text3.length() + set_text1.length(),") ");
 
 		//sort_tagを追加する条件
 		} else if(!text_copy[i].find(insert_place2)) {
 
+			//各個人の最初の項目 を検出した際会社名を更新する
+			for(int j=i;; j++) {
+
+				//社名読みを検出するまで操作します
+				if(text_copy[j].rfind(company_set) != -1) {
+
+					int len1 = text_copy[j].rfind(company_set);					//社名読みまでの先端からの長さ
+					int len2 = text_copy[j].rfind(company_search);				//改行文字の2つ後ろまでの長さ
+
+					//rfind は検出した文字の先頭を返すので検出した文字分進めるか戻す必要がある -2 は改行文字数
+					set_text1 = text_copy[j].substr(len1 + company_set.length(),len2 - len1 - company_search.length() - 2);
+
+					break;
+				}
+			}
+
+			change_text2 += set_text1;
+			change_text2 += " ";
 			change_text2 += text_copy[i + 1].substr(21);						//21はchange_text3の文字数 この後ろに苗字のフリガナがある
 			change_text2 += " ";												//空白で苗字と名前を区切ります
 			change_text2 += text_copy[i].substr(22);							//22はinsert_place2の文字数 この後ろに名前のフリガナがある
@@ -75,7 +125,7 @@ int main(int argc,const char* argv[])
 			ofstream_fail << change_text2<< " \n";
 
 			//挿入したので素のソートタグに戻します
-			change_text2 = "SORT-STRING:ゼネテック ";
+			change_text2 = "SORT-STRING:";
 		}
 
 			//配列に格納されている文字列を書き込みます
